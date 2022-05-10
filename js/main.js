1236c2d3be3284d5d605c30e8ea1bfaa0cd9657a
Slava Zharkov <vyacheslav.zharkov@instamart.ru>
2022-05-10 22:21:42 +0300

    add simple exercise

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 let body = document.getElementById('body');
 body.style['backgroundColor'] = 'lightcyan';
+
+const editor = document.getElementById('editor');
+editor.value = "select * from airplanes";
+
+const btn = document.getElementById('execute-btn');
+const result = document.getElementById('result');
+
+btn.onclick = function(event) {
+  const resultSQL = "{ id: 1 } <br/> { id: 2 } <br/> { id: 3 }";
+  result.innerHTML = resultSQL;
+}
